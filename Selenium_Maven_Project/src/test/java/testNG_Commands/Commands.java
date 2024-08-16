7ac2f12fdb5127fba224b4c64d248c3292af1f42
Devikaanju1995 <devikasatheesh888@gmail.com>
2024-08-16 15:08:57 +0530

    testngcommit4

--- Selenium_Maven_Project/src/test/java/testNG_Commands/Commands.java
+++ Selenium_Maven_Project/src/test/java/testNG_Commands/Commands.java
@@ -1,7 +1,14 @@
 package testNG_Commands;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -66,7 +73,7 @@ public void verifyRegister()
 		
 		String printtitle=driver.getTitle();
 		//System.out.println("Title is :" +printtitle);
-		String expected_title="Demo Web Shop";
+		String expected_title="Demo We Shop";
 		Assert.assertEquals(printtitle, expected_title,"Invalid title");
 	}
 	@Test
@@ -75,11 +82,11 @@ public void verifyRegister()
 		driver.get("https://demowebshop.tricentis.com/register");
 		WebElement genter_button=driver.findElement(By.id("gender-male"));
 		 boolean radiomale = genter_button.isSelected();
-		Assert.assertFalse(radiomale, "Button is not selected");
+		Assert.assertFalse(radiomale, "Button already selected");
 		
 		 genter_button.click();
 		 boolean radiomale1 = genter_button.isSelected();
-		 Assert.assertTrue(radiomale1, "Button is selected");
+		 Assert.assertTrue(radiomale1, "Button is not selected");
 		
 	}
 	@Test
@@ -89,7 +96,7 @@ public void verifyRegister()
 		WebElement suscribebutton=driver.findElement(By.id("newsletter-subscribe-button"));
 		boolean issuscribebuttonenable=suscribebutton.isEnabled();
 		
-		Assert.assertTrue(issuscribebuttonenable, "ennabled");
+		Assert.assertTrue(issuscribebuttonenable, "Not enable");
 		//String issuscribebuttonenablee="Subscribe";
 		//Assert.assertEquals(issuscribebuttonenable, "issuscribebuttonenablee");
 		
@@ -101,6 +108,34 @@ public void verifyRegister()
 		WebElement votebutton=driver.findElement(By.id("vote-poll-1"));
 		boolean isvotedisplayed=votebutton.isDisplayed();
 		//System.out.println("vote element is displayed : "+isvotedisplayed);
-		Assert.assertTrue(isvotedisplayed, "element is displayed");
+		Assert.assertTrue(isvotedisplayed, "vote button not displayed");
+	}
+	@Test
+	public void verify_javascriptexecuter_usingsendkeyandclick()
+	{
+		driver.get("https://demowebshop.tricentis.com/");
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("document.getElementById(\"newsletter-email\").value='test123@gmail.com'");
+		js.executeScript("document.getElementById(\"newsletter-subscribe-button\").click()");
+	}
+	@Test
+	public void verifyverticalscroll()
+	{
+		driver.get("https://demowebshop.tricentis.com/");
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,500)");
+	}
+	@Test
+	public void verifywaitcommands()
+	{
+		driver.get("https://demoqa.com/alerts");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//implicityWait
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("timerAlertButton")));
+		WebElement clickmefield=driver.findElement(By.id("timerAlertButton"));
+		clickmefield.click();
+		wait.until(ExpectedConditions.alertIsPresent());
+		Alert alert=driver.switchTo().alert();
+		alert.accept();
 	}
 }
